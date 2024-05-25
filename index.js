7992d99a5b2083ffafb86a45b648f8013f90443e
xRangeroQ <82023366+Ranger567@users.noreply.github.com>
2024-05-25 22:31:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,14 @@ status.forEach(s => {
   client.status(s);
 })
 client.login(process.env.BOT_TOKEN);
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running");
+});
+
+app.listen(3000, () => {
+  console.log("Server is ready.");
+});
