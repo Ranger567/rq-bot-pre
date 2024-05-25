ba7b5986541373e561e6287d3dddee41b0024335
xRangeroQ <82023366+Ranger567@users.noreply.github.com>
2024-05-25 22:06:27 +0300

    Create status.js

---
+++ status.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+  name: "Prefix => r?",
+  type: "PLAYING",
+  status: "dnd",
+  time: 12
+  },
+  {
+  name: "Owner: xRangeroQ",
+  type: "WATCHING",
+  status: "idle",
+  time: 12
+  }
+];
